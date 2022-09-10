64e93176074d3da0677248e5f5c5d3dc6d551195
AsalaTariq <112480214+AsalaTariq@users.noreply.github.com>
2022-09-10 21:07:46 +0300

    Update B8A_Group3.java

--- src/b8a_group3/B8A_Group3.java
+++ src/b8a_group3/B8A_Group3.java
@@ -11,7 +11,7 @@ public class B8A_Group3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       system.out.print("hi im asala");
     }
     
 }
