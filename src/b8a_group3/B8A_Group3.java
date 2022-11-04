4fcbce96017578e914fc55517239843b614fb0f1
Atheel_Alwathanini <atheel.2016@gmail.com>
2022-11-04 20:46:43 +0300

    Atheel Alwathanini commit

--- src/b8a_group3/B8A_Group3.java
+++ src/b8a_group3/B8A_Group3.java
@@ -5,38 +5,52 @@ package b8a_group3;
  *
  * @author asala
  */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.*;
 public class B8A_Group3 {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
        Scanner input = new Scanner(System.in);
         String[] longDonuts = {"Chocklate", "Glazed Craze "};
+        int[] longDonuts_Price = {8, 8};
         String[] fancyDonuts = {"Cinnamon Pepitas", "Strawberry Short Cake", "Orio"};
+        int[] fancyDonuts_Price = {10, 11, 11};
         String[] RegularDonuts = {"Chocklate", "Glazed Craze", "Choco Custard", "Birthday Cake"};
+        int[] RegularDonuts_Price = {6, 6, 8, 8};
 
         Customer customer1 = new Customer();
+        File Dots_Summary = new File("Dots_Summary.txt");
+        PrintWriter output = new PrintWriter(Dots_Summary);
         String type = "";
         String[] typeAndFlavor = new String[8];
-        boolean choiceFlavor = false;
+        boolean choiceFlavor = false , PaymentCheck = true;
         String[] info;
-        int t = 0;
+        int t = 0, TotalAmount = 0;
         int w = 0, choice, choice1;
 
         do {
             //first menu
-            
+            System.out.println("\n♡♡♡♡♡♡♡♡♡♡♡♡ Welcome to Dotes ♡♡♡♡♡♡♡♡♡♡♡♡");
+            System.out.println("1- Search items ♡ ");
+            System.out.println("2- Payment ♡♡♡");
+            System.out.println("3- Write a review ♡♡");
+            System.out.println("4- Show Summary ♡♡♡♡");
+            System.out.println("♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡");
+            System.out.print("Enter your choice: ");
             choice1 = input.nextInt(); //choice from the first menu
 
             switch (choice1) {
                 case 1: {
                     //Search items
                     //second menu
-                    
+                    SecondMenu();
                     //third menu
-                    info = thirdMenu(longDonuts, fancyDonuts, RegularDonuts, input);
+                    info = thirdMenu(longDonuts, fancyDonuts, RegularDonuts, input,longDonuts_Price,fancyDonuts_Price,RegularDonuts_Price);
                     choice = Integer.parseInt(info[0]); //choice from the second menue
                     if (choice == 4 || t == 8) {
                         break;
@@ -54,6 +68,7 @@ public class B8A_Group3 {
                             for (int i = 0; i < longDonuts.length; i++) {
                                 if (choice2 == (i + 1)) {
                                     typeAndFlavor[t] = type + "_" + longDonuts[i];
+                                     TotalAmount += longDonuts_Price[i];
                                     choiceFlavor = false;
                                     type = "";
                                     t++;
@@ -65,6 +80,7 @@ public class B8A_Group3 {
                             for (int i = 0; i < fancyDonuts.length; i++) {
                                 if (choice2 == (i + 1)) {
                                     typeAndFlavor[t] = type + "_" + fancyDonuts[i];
+                                    TotalAmount += fancyDonuts_Price[i];
                                     choiceFlavor = false;
                                     type = "";
                                     t++;
@@ -76,6 +92,7 @@ public class B8A_Group3 {
                             for (int i = 0; i < RegularDonuts.length; i++) {
                                 if (choice2 == (i + 1)) {
                                     typeAndFlavor[t] = type + "_" + RegularDonuts[i];
+                                    TotalAmount += RegularDonuts_Price[i];
                                     choiceFlavor = false;
                                     type = "";
                                     t++;
@@ -93,6 +110,22 @@ public class B8A_Group3 {
 
                 case 2: {
                     //Payment
+                    customer1.Payment();
+                    System.out.print("Choose your payment method:");
+                    int PaymentMethod = input.nextInt();
+                    if (PaymentMethod == 1) {
+                        customer1.Credit_Card(input);
+                        PaymentCheck = true;
+                    } else if (PaymentMethod == 2) {
+                        customer1.Cash(input, TotalAmount);
+                        PaymentCheck = false;
+                    } else {
+                        System.out.println("Wrong choice! Try again");
+                       System.out.print("Choose your payment method:");
+                        PaymentMethod = input.nextInt();
+                    }
+
+                    break;
                 }
                 
                 case 3:{
@@ -108,10 +141,25 @@ public class B8A_Group3 {
         } while (choice1 != 4 && t < 8);
 
         customer1.addOrder(typeAndFlavor);
-        ArrayList<String> orders = customer1.allOrders();     
+        ArrayList<String> orders = customer1.allOrders(); 
+        System.out.println("You can now look at your summary in the Dots Summary txt");
+        Dots_Summary(output, orders, TotalAmount, customer1, PaymentCheck);
+        output.close();
+        output.flush();
+    }
+ //---------------------------------------------------------------------------------------------------------------------------------------
+
+     public static void SecondMenu() {
+        System.out.println("\n♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡");
+        System.out.println("1 - Long Donuts");
+        System.out.println("2 - Fancy Donut");
+        System.out.println("3 - Regular Donuts");
+        System.out.println("4- Back To First Menu");
+        System.out.println("♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡");
+
     }
 //---------------------------------------------------------------------------------------------------------------------------------------
-    public static String[] thirdMenu(String[] longDonuts, String[] fancyDonuts, String[] RegularDonuts, Scanner input) {
+    public static String[] thirdMenu(String[] longDonuts, String[] fancyDonuts, String[] RegularDonuts, Scanner input,int[] longDonuts_Price , int[] fancyDonuts_Price ,int[] RegularDonuts_Price) {
         String[] info = new String[3];
         int choice;
         String type = "";
@@ -126,21 +174,21 @@ public class B8A_Group3 {
                 case 1:
                     type = "longDonuts";
                     for (int i = 0; i < longDonuts.length; i++) {
-                        System.out.println((i + 1) + ": " + longDonuts[i]+" ------- "); 
+                        System.out.println((i + 1) + ": " + longDonuts[i]+" ------- "+longDonuts_Price[i]+"$"); 
                     }
                     
                     break;
                 case 2:
                     type = "fancyDonuts";
                     for (int i = 0; i < fancyDonuts.length; i++) {
-                        System.out.println((i + 1) + ": " + fancyDonuts[i]+" ------- ");
+                        System.out.println((i + 1) + ": " + fancyDonuts[i]+" ------- "+fancyDonuts_Price[i]+"$");
                     }
                     wrongChoice = false;
                     break;
                 case 3:
                     type = "RegularDonuts";
                     for (int i = 0; i < RegularDonuts.length; i++) {
-                        System.out.println((i + 1) + ": " + RegularDonuts[i]+" ------- ");
+                        System.out.println((i + 1) + ": " + RegularDonuts[i]+" ------- "+RegularDonuts_Price[i]+"$");
                     }
                     wrongChoice = false;
                     break;
@@ -159,5 +207,26 @@ public class B8A_Group3 {
         info[1] = type;
         info[2] = flag + "";
         return info;
-    }   
+    } 
+//---------------------------------------------------------------------------------------------------------------------------------------    
+       public static void Dots_Summary(PrintWriter output, ArrayList<String> orders, int Total, Customer c, boolean test) {
+        output.print("♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡ Thank you for visiting Dots ♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡");
+        output.println("\n\n\t\t\t\tThis is your summary:");
+        output.println("Orders: ");
+        for (int i = 0; i < orders.size(); i++) {
+            output.println(orders.get(i));
+        }
+        output.println("\nYour total amount " + Total);
+        if (test == true) {
+            output.print("\nYou have chosen to pay by credit card");
+            output.print("\nYour card number: " + c.getCardNumber());
+            output.print("\nPayment status: complete ☆☆☆☆");
+        } else {
+            output.print("\nYou have chosen to pay cash");
+            output.print("\nThe name of the neighborhood: " + c.getNeighborhoodName());
+            output.print("\nThe Street Nmae:" + c.getStreetName());
+            output.print("\nPayment status: pending ☆☆☆☆");
+        }
+
+    }
 }
--- src/b8a_group3/Customer.java
+++ src/b8a_group3/Customer.java
@@ -12,6 +12,10 @@ package b8a_group3;
 import java.util.*;
 public class Customer {
     private ArrayList<String> orders = new ArrayList<String>();
+     private String FisrtStreetName , LastStreetName;  
+    private String CardNumber;
+    private String CCV;
+    private String neighborhoodName;
     
     
    //--------------------------------------------
@@ -28,5 +32,79 @@ public class Customer {
     }
     public ArrayList<String> allOrders(){
         return orders;
-    }   
+    }
+    
+    public void Payment(){
+        System.out.println("\nWelcome to the payment page ♡ ♡ ♡");
+        System.out.println("♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡");
+        System.out.println("1- Credit Card");
+        System.out.println("2 - Cash");
+        System.out.println("♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡");  
+
+    }
+    
+    public void Credit_Card(Scanner input) {
+        System.out.print("Enter Card Number: (Consists of 10 numbers) :");
+        CardNumber = input.next();
+        if (CardNumber.length() != 10) {
+            System.out.println("Invalid Card Number ! Try again");
+            System.out.println("Enter Card Number: (Consists of 10 numbers) ");
+            CardNumber = input.next();
+        }
+        System.out.print("Enter CCV Number: (Consists of 3 numbers): ");
+        CCV = input.next();
+            if (CCV.length() != 3) {
+                System.out.println("Invalid CVV number ! Try again");
+                System.out.println("Enter CCV Number: (Consists of 3 numbers)");
+                CCV = input.next();
+            }
+            System.out.println("\n☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆");  
+            System.out.println("         Payment completed");
+            System.out.println("☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆");    
+        } 
+    
+    public void Cash(Scanner input,int total){
+        if (total > 20) {
+        System.out.print("Enter the name of the neighborhood:");
+        neighborhoodName=input.next();
+        System.out.print("The Street Nmae:");
+        FisrtStreetName=input.next();
+        LastStreetName=input.next();
+        System.out.println("\n☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆");  
+        System.out.println("Your order has been confirmed");
+        System.out.println("☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆"); 
+        }else {
+        System.out.println("To use this feature, your total must be over $20");
+        System.out.println("Your Total Amount now is "+total);
+        System.out.println("We are sorry that this feature is not available to you (˃̣̣̥∩˂̣̣̥)");
+        }
+       
+         
+        
+    }
+
+    public String getNeighborhoodName() {
+        return neighborhoodName;
+    }
+   
+
+    
+
+    public String getCardNumber() {
+        return CardNumber;
+    }
+
+   
+
+    public String getCCV() {
+        return CCV;
+    }
+
+    public String getStreetName() {
+        return FisrtStreetName;
+    }
+
+    public String getLastStreetName() {
+        return LastStreetName;
+    }
 }
